3d85fffbcbc0fbc1a769e0ccedb134c535526343
Frances Morales Velilla <frances0688@gmail.com>
2023-11-13 15:32:13 +0100

    Create user logout endpoint

---
+++ middlewares/isAuthor.js
@@ -0,0 +1,22 @@
+const Post = require("../models/Post");
+
+const isAuthor = async(req, res, next) => {
+    try {
+        const post = await Post.findById(req.params._id);
+        if (post.userId.toString() !== req.user._id.toString()) {
+            return res
+                .status(403)
+                .send({message: `Post with id ${req.params._id} is not yours`});
+        }
+        next();
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .send({error, message: "There was a problem verifying the authorship of the post"});
+    }
+};
+
+module.exports = {
+    isAuthor
+};
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const {authentication, isAdmin} = require("../middlewares/authentication");
 
 router.post("/", UserController.register);
 router.post("/login", UserController.login);
+router.delete("/logout", authentication, UserController.logout);
 
 module.exports = router;
